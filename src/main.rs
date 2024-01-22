4c11e6d4d54644d91daf1f9cd2acd242b2200e59
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-01-22 00:01:22 -0300

    separate Calculator logic from main App

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,22 @@ impl Op {
     }
 }
 
-// define a component that renders a div with the text "Hello, world!"
+// Main application component
 fn App(cx: Scope) -> Element {
     let window = use_window(cx);
     window.set_title("Calculator");
+
+    cx.render(rsx! {
+        h1 { "Calculator" }
+        p {
+            "Type it"
+        }
+        Calculator {}
+    })
+}
+
+// Calculator logic
+fn Calculator(cx: Scope) -> Element {
     // Currently visible input.
     let input = use_state::<i64>(cx, || 0);
     // If input should be cleared on the next digit.
@@ -95,150 +107,144 @@ fn App(cx: Scope) -> Element {
         operation.set(Some(op));
     };
     cx.render(rsx! {
-        div {
-            h1 { "Calculator" }
-            p {
-                "Type it"
-            }
-            input {
-                value: "{input}",
-                prevent_default: "onkeydown",
-                onkeydown: move |evt| {
-                    if let Key::Character(key) = evt.key() {
-                        if let Ok(digit) = key.parse::<u8>() {
-                            if digit < 10 {
-                                push_digit(digit);
-                            }
-                        } else if key == "-" {
-                            if get_input_or_clear() == 0 {
-                                // negative number
-                                input.set(-1)
-                            }
+        input {
+            value: "{input}",
+            prevent_default: "onkeydown",
+            onkeydown: move |evt| {
+                if let Key::Character(key) = evt.key() {
+                    if let Ok(digit) = key.parse::<u8>() {
+                        if digit < 10 {
+                            push_digit(digit);
                         }
-                    } else if let Key::Backspace = evt.key() {
-                        // remove a digit from the end
-                        let current = input.get();
-                        input.set(current / 10);
+                    } else if key == "-" {
+                        if get_input_or_clear() == 0 {
+                            // negative number
+                            input.set(-1)
+                        }
                     }
-                },
-            }
-            div {
-                display: "grid",
-                // 3 columns, N rows grid
-                grid_template_columns: "repeat(3, 2rem)",
-                grid_auto_flow: "row",
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(1),
-                    "1"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(2),
-                    "2"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(3),
-                    "3"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(4),
-                    "4"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(5),
-                    "5"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(6),
-                    "6"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(7),
-                    "7"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(8),
-                    "8"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(9),
-                    "9"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| pop_digit(),
-                    "D"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_digit(0),
-                    "0"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| {
-                        apply_op();
-                    },
-                    "="
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| {
-                        reset_op();
-                    },
-                    "C"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_op(Op::Add),
-                    "+"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_op(Op::Sub),
-                    "-"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_op(Op::Mul),
-                    "*"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_op(Op::Div),
-                    "/"
-                },
-                button {
-                    background_color: "blue",
-                    color: "white",
-                    onclick: move |_| push_op(Op::Pow),
-                    "**"
+                } else if let Key::Backspace = evt.key() {
+                    // remove a digit from the end
+                    let current = input.get();
+                    input.set(current / 10);
                 }
+            },
+        }
+        div {
+            display: "grid",
+            // 3 columns, N rows grid
+            grid_template_columns: "repeat(3, 2rem)",
+            grid_auto_flow: "row",
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(1),
+                "1"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(2),
+                "2"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(3),
+                "3"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(4),
+                "4"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(5),
+                "5"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(6),
+                "6"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(7),
+                "7"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(8),
+                "8"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(9),
+                "9"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| pop_digit(),
+                "D"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_digit(0),
+                "0"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| {
+                    apply_op();
+                },
+                "="
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| {
+                    reset_op();
+                },
+                "C"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_op(Op::Add),
+                "+"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_op(Op::Sub),
+                "-"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_op(Op::Mul),
+                "*"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_op(Op::Div),
+                "/"
+            },
+            button {
+                background_color: "blue",
+                color: "white",
+                onclick: move |_| push_op(Op::Pow),
+                "**"
             }
         }
     })
